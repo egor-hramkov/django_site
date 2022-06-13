5d4797e4f55ba05f31d319431adc997d90b4f018
egor-hramkov <egor_kuplinov@mail.ru>
2022-06-13 21:03:47 +0400

    added templates

--- first_project/news/views.py
+++ first_project/news/views.py
@@ -1,17 +1,21 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 
+from .models import *
+
+menu = ['Войти', 'Добавить новость', 'О сайте']
 
 def index(request):
-    return HttpResponse("<h1>Страница с новостями</h1>")
+    posts = News.objects.all()
+    return render(request, 'news/news.html', {'title': 'Новости', 'menu': menu, 'posts': posts})
 
 def newsById(request, news_id):
-    if news_id > 10:
+    if news_id < 1:
         return redirect('home')
-    return HttpResponse(f"<h1>Страница {news_id} новости<h1>")
+    return render(request, 'news/news_byId.html', {'title': str(news_id) + ' Новость', 'menu': menu})
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Данная страница не найдена</h1>')
 
 def home(request):
-    return HttpResponse("<h1>Главная страница</h1>")
+    return render(request, 'news/index.html', {'title': 'Главная страница', 'menu': menu})
