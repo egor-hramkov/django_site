33244758132a9332e9af0b78a7591e51d54163f7
Egor Hramkov <hsg1972@mail.ru>
2022-06-22 21:31:13 +0300

    add .gitignore

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mqf+m17+8%k)=677-7uj2vnt*2*rzy_k6)sktj--5-2g*@wc70'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
