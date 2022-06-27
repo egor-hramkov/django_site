5f0bbefffe11f32992403de528cfe764fa4f614f
Egor Hramkov <hsg1972@mail.ru>
2022-06-27 18:57:20 +0300

    add mixin class, add pagination

---
+++ first_project/news/utils.py
@@ -0,0 +1,15 @@
+from .models import *
+
+menu = [
+    {'title': 'На главную', 'url_name': 'home'},
+    {'title': 'Новости', 'url_name': 'news'},
+    {'title': 'Опубликовать новость', 'url_name': 'addNews'}
+]
+
+class DataMixin:
+    def get_user_context(self, **kwargs):
+        context = kwargs
+        cats = Category.objects.all()
+        context['menu'] = menu
+        context['cats'] = cats
+        return context
--- first_project/news/views.py
+++ first_project/news/views.py
@@ -1,43 +1,43 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.views.generic import ListView, DetailView, CreateView
 
 from .forms import *
 from .models import *
+from .utils import DataMixin, menu
 
-menu = [{'title': 'На главную', 'url_name': 'home'}, {'title': 'Новости', 'url_name': 'news'}, {'title': 'Опубликовать новость', 'url_name': 'addNews'}]
 
 def home(request):
     return render(request, 'news/index.html', {'title': 'Главная страница', 'menu': menu})
 
-class AllNews(ListView):
+class AllNews(DataMixin, ListView):
     model = News
     template_name = "news/news.html"
     context_object_name = 'posts'
+    paginate_by = 3
 
     def get_context_data(self, *,object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Новости'
-        context['menu'] = menu
-        context['cats'] = Category.objects.all()
         context['form'] = SearchNews()
-        return context
+        mix_def = self.get_user_context(title='Новости')
+        return dict(list(context.items()) + list(mix_def.items()))
 
-class NewsByCat(ListView):
+class NewsByCat(DataMixin, ListView):
     model = News
     template_name = "news/newsByCat.html"
     context_object_name = 'news'
+    paginate_by = 3
 
     def get_context_data(self, *,object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Новости по категориям ' + str(self.kwargs['cat_slug'])
-        context['menu'] = menu
-        return context
+        mix_def = self.get_user_context(title='Новости по категориям ' + str(self.kwargs['cat_slug']))
+        return dict(list(context.items()) + list(mix_def.items()))
 
     def get_queryset(self):
         return News.objects.filter(cat__slug=self.kwargs['cat_slug']).all()
 
-class newsById(DetailView):
+class newsById(DataMixin, DetailView):
     model = News
     template_name = "news/newsById.html"
     pk_url_kwarg = 'news_id'
@@ -45,18 +45,16 @@ class newsById(DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = str(self.kwargs['news_id']) + ' Новость'
-        context['menu'] = menu
-        return context
+        mix_def = self.get_user_context(title=str(self.kwargs['news_id']) + ' Новость')
+        return dict(list(context.items()) + list(mix_def.items()))
 
-class AddNews(CreateView):
+class AddNews(DataMixin, CreateView):
     form_class = AddNewsForm
     template_name = 'news/addNews.html'
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = "Добавление новости"
-        context['menu'] = menu
-        return context
+        mix_def = self.get_user_context(title="Добавление новости")
+        return dict(list(context.items()) + list(mix_def.items()))
 
 def pageNotFound(request, exception):
     context = {
@@ -71,18 +69,29 @@ def searchNewsBy(request):
         form = SearchNews(request.POST)
         if form.is_valid():
             data = form.cleaned_data.get("searchBy")
+            request.session['search'] = data
         else:
             data=""
         n = News.objects.filter(title__iregex=data).all()
-        context = {
-            'news': n,
-            'form': form,
-            'title': 'Новости',
-            'menu': menu
-        }
-        return render(request, 'news/newsBySearch.html', context=context)
     else:
-        return redirect('home')
+        if 'search' in request.session:
+            n = News.objects.filter(title__iregex=request.session['search']).all()
+
+    paginator = Paginator(n, 3)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'paginator': paginator,
+        'news': n,
+        'form': form,
+        'title': 'Новости',
+        'menu': menu
+    }
+    return render(request, 'news/newsBySearch.html', context=context)
+
 
 # def news(request):
 #     posts = News.objects.all()
