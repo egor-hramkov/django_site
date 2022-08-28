43244fa6e391646c2d9ce0c40af40dd36e8170ab
Egor <egor_kuplinov@mail.ru>
2022-08-28 20:52:37 +0400

    revamped project by adding dir account

--- first_project/news/models.py
+++ first_project/news/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from account.models import Profile
 
 # Create your models here.
 
@@ -36,22 +37,6 @@ class Category(models.Model):
         verbose_name = 'Категории'
         verbose_name_plural = 'Категории'
         ordering = ['id']
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    about = models.TextField(null=True, blank=True)
-    profile_pic = models.ImageField(null=True, blank=True, upload_to="photos/", default="photos/user.png", verbose_name='Фотография пользователя')
-
-    class Meta:
-        verbose_name = 'Профиль'
-        verbose_name_plural = 'Профили'
-        ordering = ['id']
-
-    def __str__(self):
-        return self.user.username
-
-    def get_absolute_url(self):
-        return reverse('profile', kwargs={'profile_id': self.pk})
 
 class UserFollowing(models.Model):
     user = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE, null=True)
